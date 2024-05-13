7316b481e4d18686d4b246190c91bffc402b645e
Steve Schwab <schwabs@umich.edu>
2024-05-13 16:06:27 -0700

    add friend property to the request

--- api/server.js
+++ api/server.js
@@ -13,16 +13,22 @@ function shortCircuit(req, res, next) {
   res.json('the request was short circuited!');
 }
 
+function addFriend(req, res, next) {
+  req.friend = 'Lady Gaga';
+  next();
+}
+
 server.use(express.json());
 server.use(morgan('dev'));
 server.use(customMorgan);
-server.use(shortCircuit);
+// server.use(shortCircuit);
+server.use(addFriend);
 
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
   res.send(`
-    <h2>Hubs API</h2>
+    <h2>Hubs API ${req.friend}</h2>
     <p>Welcome to the Hubs API</p>
   `);
 });
