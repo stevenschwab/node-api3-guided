69b44aba29a7931fed8d37ac36665ee5d629961e
Steve Schwab <schwabs@umich.edu>
2024-05-13 15:25:56 -0700

    require morgan to log server

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const express = require('express'); // importing a CommonJS module
-
+const morgan = require('morgan');
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
